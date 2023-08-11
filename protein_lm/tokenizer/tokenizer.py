3fbc573a37377912c882071116b0e166fac0de21
jamaliki <kjamali@mrc-lmb.cam.ac.uk>
2023-08-10 22:38:42 -0700

    Add our own tokenizer, plus padding and batch encoding support

--- protein_lm/tokenizer/tokenizer.py
+++ protein_lm/tokenizer/tokenizer.py
@@ -1,3 +1,4 @@
+import torch
 from typing import List
 
 from rust_trie import Trie 
@@ -12,9 +13,40 @@ class Tokenizer:
         # The Trie will return len(self.ids_to_tokens) when it can't find the token
         # For decoding, we need to add the <unk> token back in, even if it already exists
         self.ids_to_tokens += ["<unk>"]
+        self.pad_token_id = self.ids_to_tokens.index("<pad>")
+        self.mask_token_id = self.ids_to_tokens.index("<mask>")
 
-    def encode(self, sequence: str) -> List[int]:
-        return self.trie.tokenize(sequence)
+    def encode(
+        self, 
+        sequence: str, 
+        add_special_tokens: bool = False,
+        return_tensor: bool = False,
+    ) -> List[int]:
+        if add_special_tokens:
+            sequence = "<cls>" + sequence + "<eos>"
+        output = self.trie.tokenize(sequence)
+        if return_tensor:
+            output = torch.tensor(output, dtype=torch.long)
+        return output
+
+    def batch_encode(
+        self,
+        sequences: List[str],
+        add_special_tokens: bool = False,
+        return_tensors: bool = False,
+        max_sequence_length: int = None,
+    ) -> List[List[int]]:
+        output = []
+        if max_sequence_length is not None:
+            sequences = [sequence[:max_sequence_length] for sequence in sequences]
+        for sequence in sequences:
+            output.append(self.encode(sequence, add_special_tokens, return_tensors))
+        if return_tensors:
+            tensor_out = torch.full((len(output), max_sequence_length), self.pad_token_id)
+            for i, sequence in enumerate(output):
+                tensor_out[i, :len(sequence)] = sequence
+            output = tensor_out
+        return output
 
     def decode(self, tokens: List[int]) -> str:
         return "".join([self.ids_to_tokens[idx] for idx in tokens])
@@ -31,3 +63,16 @@ class EsmTokenizer(Tokenizer):
         super().__init__(self.ids_to_tokens)
 
 
+
+class AptTokenizer(Tokenizer):
+    def __init__(self, tokens: List[str]):
+        # For our own tokenizers, we don't need to explicitly add the <unk> token
+        # because it gets added as the last token in the tokens list
+        # I've also removed X so that it gets translated to <unk>
+        self.ids_to_tokens = [
+            "<cls>", "<pad>", "<eos>", "L", "A", "G", "V", 
+            "S", "E", "R", "T", "I", "D", "P", "K", "Q", "N", 
+            "F", "Y", "M", "H", "W", "C", "B", "U", "Z", "O", 
+            "<mask>"
+        ]
+        super().__init__(tokens)
