1a4d449ab1f43f7578fc888e6d65696c8824c945
othertea <124535597+othertea@users.noreply.github.com>
2023-08-25 07:39:14 -0700

    feat: initial restructure of training script (#26)

---
+++ protein_lm/modeling/getters/model.py
@@ -0,0 +1,39 @@
+from typing import Dict, Literal, Optional
+
+import torch
+from pydantic import BaseModel
+
+from protein_lm.modeling.models.apt.config import APTConfig
+from protein_lm.modeling.models.apt.model_pytorch import APTLMHeadModel
+
+
+class NNModelConfig(BaseModel):
+    # If desired, this can be modified to support a variety of model types
+    # Note: "nn_model_.." because anything with the "model_" prefix leads to
+    # pydantic namespace warnings
+    nn_model_type: Literal["APT"]
+    nn_model_config_args: Dict
+    pretrained_checkpoint: Optional[str]
+
+
+def get_model(config_dict: Dict):
+    config = NNModelConfig(**config_dict)
+    if config.nn_model_type == "APT":
+        model_constructor = APTLMHeadModel
+        model_config_constructor = APTConfig
+    else:
+        raise ValueError(f"Invalid NNModelConfig.nn_model_type {config.nn_model_type}")
+
+    model_config = model_config_constructor(**config.nn_model_config_args)
+    if config.pretrained_checkpoint is None:
+        model = model_constructor(config=model_config)
+    else:
+        model = model_constructor.from_pretrained(
+            pretrained_model_name_or_path=config.pretrained_checkpoint,
+            config=model_config,
+        )
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    return model
---
+++ protein_lm/modeling/getters/training_args.py
@@ -0,0 +1,45 @@
+import os
+from typing import Dict, List, Union
+
+from pydantic import BaseModel, FieldValidationInfo, field_validator
+from transformers import TrainingArguments
+
+
+class TrainingArgsConfig(BaseModel):
+    per_device_train_batch_size: int
+    learning_rate: float
+    weight_decay: float
+    num_train_epochs: int
+    max_steps: int
+    save_steps: int
+    output_dir: str
+    save_strategy: str
+    report_to: str
+    label_names: List[str]
+    no_cuda: bool
+
+    @field_validator(
+        "per_device_train_batch_size",
+        "num_train_epochs",
+        "max_steps",
+        "weight_decay",
+        "learning_rate",
+        "save_steps",
+    )
+    @classmethod
+    def check_gt_zero(cls, v: Union[int, float], info: FieldValidationInfo):
+        if v <= 0:
+            raise ValueError(f"trainer.{info.field_name} must be greater than 0")
+        return v
+
+
+def get_training_args(config_dict: Dict) -> TrainingArguments:
+    config = TrainingArgsConfig(**config_dict)
+
+    if not os.path.isdir(config.output_dir):
+        print(f"creating checkpoint directory at {config.output_dir}")
+        os.makedirs(config.output_dir)
+
+    return TrainingArguments(
+        **config_dict,
+    )
