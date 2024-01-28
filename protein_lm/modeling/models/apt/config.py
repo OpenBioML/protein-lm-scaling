f751782797f61d314095d96cfd4a0aa98ccbf836
othertea <124535597+othertea@users.noreply.github.com>
2024-01-27 16:27:44 -0800

    fix: rope options and attn_type

--- protein_lm/modeling/models/apt/config.py
+++ protein_lm/modeling/models/apt/config.py
@@ -1,5 +1,5 @@
 from transformers import GPT2Config
-
+from typing import Literal
 
 class APTConfig(GPT2Config):
     """
@@ -8,9 +8,10 @@ class APTConfig(GPT2Config):
 
     def __init__(
         self,
-        position_embedding="learned",
+        position_embedding: Literal["alibi", "learned", "rope", "rerope", "linear_rope_scaling", "dynamic_rope_scaling"]="learned",
         tokenizer=None,
         max_sequence_length = 1024,
+        attn_type="standard",
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -18,4 +19,5 @@ class APTConfig(GPT2Config):
         self.position_embedding = position_embedding
         self.tokenizer = tokenizer
         self.max_sequence_length = max_sequence_length
+        self.attn_type = attn_type
 
